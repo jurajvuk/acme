9c579b350412d69db5be8d1c95631f0f6cc23011
Juraj Vukic <jurajvuk@gmail.com>
2015-12-22 20:22:31 +0000

    simplified code

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -20,39 +20,33 @@ class Validator
 				
 				$exploded = explode(":", $rule);
 
-				if (isset($_POST[$name])) {
-					switch ($exploded[0]) {
-						case 'min':
-							$min = $exploded[1];
-							if (Valid::alpha()->length($min)->Validate($_POST[$name]) == false) {
-								$errors[] = $name . " must be at least " . $exploded[1] . " characters long!";
-							}
-							break;
+				switch ($exploded[0]) {
+					case 'min':
+						$min = $exploded[1];
+						if (Valid::alpha()->length($min)->Validate($_POST[$name]) == false) {
+							$errors[] = $name . " must be at least " . $exploded[1] . " characters long!";
+						}
+						break;
 						
-						case 'email':
-							if (Valid::email()->validate($_POST[$name]) == false) {
-								$errors[] = $name . " must be a valid email address!";
-							}
-							break;
+					case 'email':
+						if (Valid::email()->validate($_POST[$name]) == false) {
+							$errors[] = $name . " must be a valid email address!";
+						}
+						break;
 
-						case 'equalTo':
-							if (Valid::equals($_POST[$name])->validate($_POST[$exploded[1]]) == false) {
-								$errors[] = $name . "'s don't match!";
-							}
+					case 'equalTo':
+						if (Valid::equals($_POST[$name])->validate($_POST[$exploded[1]]) == false) {
+							$errors[] = $name . "'s don't match!";
+						}
 							
 							
-							break;
+						break;
 
-						default:
-							# code...
-							break;
-					}
-				} else {
-					$errors[] = "No value found!";
+					default:
+						$errors[] = "No value found!";
+						break;
 				}
-
-			
-			
+				
 			}
 
 		}
